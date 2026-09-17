61448506935b54eea70dcfd0db42498d9334a968
80ie <25000kittens@gmail.com>
2026-09-17 14:25:13 -0700

    sscratch

--- scratch.glsl
+++ scratch.glsl
@@ -5,7 +5,7 @@ uniform vec2 u_mouse;
 uniform float u_time;
 
 const float GRID_SIZE   = 1.0;  // cells across the short axis
-const vec2 SIZE = vec2(0.2);
+const vec2 SIZE = vec2(0.1);
 
 vec2 toCanvasSpace(vec2 pixelCoord)
 {
@@ -28,13 +28,15 @@ void main()
     vec2 uv = toCanvasSpace(gl_FragCoord.xy);
     vec2 mouse = toCanvasSpace(u_mouse); 
 
-    float box = sdRoundedBox(uv, vec2(SIZE.x*aspect, SIZE.y), vec4(0.0));
-    box = step(box, 0.15);
-    //float glow = 1.0 - smoothstep(0.0, 3.0, length());
+    float box = sdRoundedBox(uv, vec2(SIZE), vec4(0.0));
+    //box = step(box, 0.15);
+
+    float glow = 1.0 - smoothstep(0.0, 1.0, length(uv - mouse));
+
     float dst = smoothstep(0.0, 0.5, length(uv - mouse));    
 
-
+    float intensity = 1.0 - box*glow;
     vec3 color = vec3(box);
-    color *= dst;
+    //color *= dst;
     gl_FragColor = vec4(color, 1.0);
 }
