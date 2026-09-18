2e65cb98d40db1e8b58cc8494ff634813866528c
80ie <25000kittens@gmail.com>
2026-09-18 11:44:30 -0700

    kishimisu yt tutorial

--- kishimisu.glsl
+++ kishimisu.glsl
@@ -6,17 +6,24 @@ uniform float u_time;
 
 const float freq = 7.0;
 
-vec3 palette( float t, vec3 a, vec3 b, vec3 c, vec3 d )
+vec3 palette( float t )
 {
+    // 0.5, 0.5, 0.5		0.5, 0.5, 0.5	1.0, 1.0, 1.0	0.00, 0.10, 0.20
+    vec3 a = vec3(0.5);
+    vec3 b = vec3(0.5);
+    vec3 c = vec3(1.0);
+    vec3 d = vec3(0.0, 0.32, 0.67);	
+
     return a + b*cos( 6.28318*(c*t+d) );
 }
 
 void main() 
 {
     vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / u_resolution.y;
-    vec3 col = vec3(0.0, 0.0, 0.0); 
 
     float d = length(uv);
+
+    vec3 col = palette(fract(u_time*0.05)); 
 
     d = sin(d*freq - u_time)/freq;
     d = abs(d);
@@ -24,7 +31,7 @@ void main()
     
     //d = 0.02 / d; 
 
-    col += d;
+    //col += d;
 
     gl_FragColor = vec4(col, 1.0);
 }
